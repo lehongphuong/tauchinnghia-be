bbad13ee846be65dd9deaef3add1d9e5a779595b
Le Hong Phuong <helloitpdu@gmail.com>
2019-12-10 22:43:51 +0700

    PhuongLH5: commit all file at 12/10/2019 : 22:43:51.11

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -93,15 +93,14 @@ TEMPLATES = [
 ]
  
 WSGI_APPLICATION = 'djangoherokuapp.wsgi.application'
-# Email settings
+# Email settings 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'lehongphuongcntt@gmail.com'
-EMAIL_HOST_PASSWORD = 'Professionaltools'
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
-EMAIL_PORT = '587'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'lehongphuongcntt@gmail.com'
+EMAIL_HOST_PASSWORD = 'Professionaltools'  
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
